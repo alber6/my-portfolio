7ff8ced3e8e0b4ea2c00246260901885e72da339
alber6 <143524073+alber6@users.noreply.github.com>
2025-03-09 20:59:01 +0100

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,12 +6,12 @@ export const Footer = () => `
 <h2>Contact</h2>
 <div>
 <a href= https://github.com/ target="_blank">
-${Button("/public/github.jpg", "GitHub")}
+${Button("/public/images/github.jpg", "GitHub")}
 </a>
 <a href= https://www.linkedin.com/in/alberto-peinado-gonz%C3%A1lez-794664110/ target="_blank">
 ${Button("/public/linkedin.jpg", "LinkedIn")}
 <a href="mailto:albertopeinado08@gmail.com">
-${Button("/public/gmail.png", "Gmail")}
+${Button("/public/images/gmail.png", "Gmail")}
 </a>
 </div>
 `;
