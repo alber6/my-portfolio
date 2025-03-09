9358d9d929a352a1d425aa1aed3bf26dfa645473
alber6 <143524073+alber6@users.noreply.github.com>
2025-03-09 20:47:54 +0100

    links modified

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
       title: "Primer proyecto",
       description: "Star Wars: Jedi Survivor - Flexbox",
       image:
-        "public/project1.Jedi.png/",
+        "/public/images/project1.Jedi.png",
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/alber6/Flexbox/blob/main/index.html",
       link: "https://jedisurvivor.vercel.app/",
@@ -12,7 +12,7 @@ export const projects = [
       title: "Segundo proyecto",
       description: "M.Jackson - Flex-Grid",
       image:
-        "/public/project2.MJ.png/",
+        "/public/images/project2.MJ.png/",
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/alber6/M.Jackson/blob/main/style.css",
       link: "https://michaeljackson-chi.vercel.app/",
@@ -21,7 +21,7 @@ export const projects = [
       title: "Tercer proyecto",
       description: "Carhartt - Práctica de todo lo estudiado",
       image:
-        "public/project3.Carhartt.png",
+        "/public/images/project3.Carhartt.png/",
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/alber6/Proyecto1",
       link: "https://carhartt-eight.vercel.app/",
@@ -30,7 +30,7 @@ export const projects = [
       title: "Cuarto proyecto",
       description: "Tienda - Trabajando con DOM",
       image:
-        "/public/projectDOM.png",
+        "/public/images//projectDOM.png/",
       tech: ["HTML5", "CSS3", "JavaScript"],
       github: "https://github.com/alber6/PracticaDOM/blob/main/main.js",
       link: "https://practica-dom-dusky.vercel.app/",
